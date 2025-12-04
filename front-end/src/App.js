39821631d4c0b30de413c125e22704861c01cb9a
PHB-1994 <gudqls0991@naver.com>
2025-12-04 18:10:47 +0900

    프론트엔드 환경 설정

---
+++ front-end/src/App.js
@@ -0,0 +1,38 @@
+// ============================================
+// src/App.js
+// TODO: React Router 설정하기
+// - BrowserRouter, Routes, Route import 하기
+// - LoginPage, FeedPage, UploadPage import 하기
+// - PrivateRoute import 하기
+// - /login 경로에 LoginPage 연결
+// - /feed 경로에 FeedPage 연결 (PrivateRoute로 보호)
+// - /upload 경로에 UploadPage 연결 (PrivateRoute로 보호)
+// - 기본 경로(/)는 /login으로 리다이렉트
+// ============================================
+
+import React from 'react';
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import FeedPage from "./pages/FeedPage";
+import LoginPage from "./pages/LoginPage";
+import UploadPage from "./pages/UploadPage";
+
+// TODO: 필요한 컴포넌트들을 import 하세요
+
+function App() {
+    return (
+        <div>
+            {/* TODO: Router 설정을 완성하세요 */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginPage/>}/>
+                    <Route path="/feed" element={<FeedPage/>}/>
+                    <Route path="/login" element={<LoginPage/>}/>
+                    <Route path="/upload" element={<UploadPage/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
